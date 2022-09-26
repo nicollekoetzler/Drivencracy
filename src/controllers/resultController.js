cb9962ef4def91c20f03e2350c6d6dce26e4e510
nicollekoetzler <nickkoetzler@gmail.com>
2022-09-25 21:55:26 -0300

    retorna resultado da enquete

--- src/controllers/resultController.js
+++ src/controllers/resultController.js
@@ -1,3 +1,46 @@
+import db from "../databases/mongo.js";
+import { ObjectId } from "mongodb";
+
 export async function getResult (req, res){
-    
+
+    try{
+
+        const id = req.params.id;
+
+        const poll = await db.collection("poll").findOne({ _id: ObjectId(id) });
+
+        const options = await db.collection("choice").find({ pollId: id }).toArray()
+
+        const votesQuantity = []
+        
+        for(let i = 0; i < options.length ; i++){
+            const votes = await db.collection("vote").find({ choiceId: options[i]._id }).toArray()
+            votesQuantity.push(votes.length)
+        }
+
+        console.log(votesQuantity)
+
+        let mostVotes = votesQuantity[0]
+        let mostVotedOption = options[0]
+
+        for(let i = 0; i < votesQuantity.length; i++){
+            if(votesQuantity[i] > mostVotes){
+                mostVotes = votesQuantity[i]
+                mostVotedOption = options[i]
+            }
+        }
+
+        res.status(200).send({
+            title: poll.title,
+            expireAt: poll.expireAt,
+            result: {
+                title: mostVotedOption.title,
+                votes: mostVotes
+            }
+        });
+
+    } catch (err){
+        console.log(err)
+        res.sendStatus(500)
+    }
 }
